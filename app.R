3b97945ca212eb01012eb87e2fc98b0ba1fe241f
Pedro Florencio <pedroflorencio@Pedros-MacBook-Air.local>
2024-12-24 18:42:46 -0300

    :construction: adicionando mapa e estilos

--- app.R
+++ app.R
@@ -1,12 +1,24 @@
-install.packages("shiny")
 library(shiny)
+library(leaflet)
 
-ui <- fluidPage(
-  "Hello, World!"
-)
+ui <- navbarPage("Instituto de Pesquisa e Planejamento de Fortaleza", id='nav',
+             tabPanel('Mapa',
+                      div(class='outer',
+                          tags$head(
+                            includeCSS("styles.css"),
+                            includeScript("gomap.js")
+                                    ),
+                          leafletOutput("map", width="100%", height="100%")
+                          )
+                      )
+                  )
 
 server <- function(input, output, session) {
-  
+  output$map <- renderLeaflet(
+    leaflet() %>% 
+      addProviderTiles("CartoDB") %>% 
+      setView(lng=-38.5279, lat=-3.7327, zoom=11)
+  )
 }
 
 shinyApp(ui, server)
